bafa6d1b53f28a71160225c426cc9015e9ebfa1e
QarthO <qartho@gmail.com>
2025-05-10 06:31:32 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+    // This file is not needed for a simple webview of a remote URL.
+    // The window configuration in tauri.conf.json handles loading the URL.
+    return null;
+  }
+  
+  export default App;
+  
